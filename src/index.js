ff626156b11272b9a023bbc6e1c6319147e8d6a1
David Omstead <davidomstead@hotmail.com>
2019-06-17 12:00:37 -0700

    Reconfigured

---
+++ src/index.js
@@ -0,0 +1,22 @@
+
+import $ from 'jquery';
+// import './normalize.css';
+import store from './store';
+import shoppingList from './shopping-list';
+import api from './api';
+import './index.css';
+
+/* global shoppingList, store, api */
+
+$(document).ready(function() {
+  shoppingList.bindEventListeners();
+
+  // On initial load, fetch Shopping Items and render
+  api.getItems()
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    })
+    .catch(err => console.log(err.message));
+});
+
